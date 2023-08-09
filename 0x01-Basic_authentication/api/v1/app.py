2b099f8ec7cbf8c125d8ffa8d177331635a3056d
Denniskwg <dkamau476@gmail.com>
2023-08-09 15:03:05 +0300

    corrections

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -53,10 +53,10 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     var = getenv("AUTH_TYPE", None)
-    if var == 'auth':
-        from api.v1.auth.auth import Auth
-        auth = Auth()
     if var == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
     app.run(host=host, port=port)
