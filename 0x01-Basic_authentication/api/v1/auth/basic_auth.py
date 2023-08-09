47ef7e24d79970d26911b8a6dfc4c3be9cc8c185
Denniskwg <dkamau476@gmail.com>
2023-08-09 15:17:34 +0300

    adding basic auth base64 functionality

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,16 @@ class BasicAuth(Auth):
     """inherits from Auth and defines a BasicAuth
     class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        lst = authorization_header.split()
+        if len(lst) != 2:
+            return None
+        else:
+            if lst[0] == 'Basic':
+                return lst[1]
