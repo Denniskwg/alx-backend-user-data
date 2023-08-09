d99dce8bf495d8e39f1b64d9f34ca9b06b0f1b6c
Denniskwg <dkamau476@gmail.com>
2023-08-09 16:38:55 +0300

    adding function that extracts user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,19 @@ class BasicAuth(Auth):
         except base64.binascii.Error:
             return None
         return decoded.decode()
+
+    def extract_user_credentials(
+        self,
+        decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """ returns the user email and password from the
+        Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        lst = decoded_base64_authorization_header.split(':')
+        return (lst[0], lst[1])
