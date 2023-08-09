bf5562679d3e406308ff1dc0eef6ce9b52817015
Denniskwg <dkamau476@gmail.com>
2023-08-09 16:26:57 +0300

    adding basic auth base64 decode function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,14 +2,17 @@
 """basic_auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """inherits from Auth and defines a BasicAuth
     class
     """
-    def extract_base64_authorization_header(self, 
-            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
         """returns the Base64 part of the Authorization header
         """
         if authorization_header is None:
@@ -22,3 +25,19 @@ class BasicAuth(Auth):
         else:
             if lst[0] == 'Basic':
                 return lst[1]
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+    ) -> str:
+        """decodes a header to base64
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+        except base64.binascii.Error:
+            return None
+        return decoded.decode()
