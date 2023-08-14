ae829bd787dc32b9af5bf7aa22829ec778576a87
Denniskwg <dkamau476@gmail.com>
2023-08-14 12:02:57 +0300

    corrections

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,11 +23,9 @@ def before_each_request():
     paths.append('/api/v1/auth_session/login/')
     if auth:
         if auth.require_auth(request.path, paths):
-            res = auth.authorization_header(request)
-            res2 = auth.session_cookie(request)
-            if res is None and res2 is None:
-                abort(401)
-            if auth.authorization_header(request) is None:
+            res1 = auth.session_cookie(request)
+            res2 = auth.authorization_header(request)
+            if res1 is None and res2 is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
